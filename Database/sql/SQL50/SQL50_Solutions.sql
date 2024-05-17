e49f8c87d63209fa5510ff992102e0cecd4f2e81
Vinu Karthek <VinuKarthek.Rajamani@infineon.com>
2024-05-17 23:36:36 +0800

    SQL 50 Question Leetcode

---
+++ Database/sql/SQL50/SQL50_Solutions.sql
@@ -0,0 +1,104 @@
+-- Q1 1757. Recyclable and Low Fat Products
+-- SELECT product_id from Products WHERE low_fats = 'Y' AND recyclable = 'Y'
+
+-- Q2 584. Find Customer Referee
+-- SELECT name from Customer WHERE referee_id is NULL or referee_id  != 2
+
+-- Q3 595. Big Countries
+-- SELECT name, population, area from World WHERE area >= 3000000 OR population >= 25000000
+
+-- Q4 1148. Article Views I
+-- SELECT DISTINCT(author_id) AS id 
+-- FROM Views WHERE author_id = viewer_id 
+-- ORDER BY id ASC;
+
+-- Q5 1683. Invalid Tweets
+-- SELECT tweet_id FROM Tweets WHERE length(content)>15;
+
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
+-- Q6
+-- 
+
+-- Q7
+-- 
+
+-- Q8
+-- 
